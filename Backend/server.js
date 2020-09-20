c34a4f1a6efa1b327de585c463345ea7b72da5de
Ismael Muñoz <Prosilver05@gmail.com>
2020-09-20 17:49:40 -0500

    added env variables

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+require("dotenv").config()
+
 let port = process.env.PORT || 5000
 
 let Users = [
@@ -10,11 +12,25 @@ let Users = [
     {"name": "Euge"}
 ]
 
-app.use(express.static(path.resolve(__dirname, "../build")))
 
-app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"))
-})
+if (process.env.URL_THINGY === "coso"){
+
+    app.use(express.static(path.resolve(__dirname, "../build")))
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+    })
+
+} else {
+
+    app.use(express.static(path.resolve(__dirname, "./coso.html")))
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "coso.html"))
+    })
+
+}
+
 
 app.get("/users-info", (req,res) => {
     res.send(Users)
